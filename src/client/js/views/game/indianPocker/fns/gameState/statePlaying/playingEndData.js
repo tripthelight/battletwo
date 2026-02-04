eb9e9b2bc255162fbff6972ab7a25c099d21be6a
tripthelight <bye_ok@naver.com>
2026-02-04 15:48:49 +0900

    [260204] - 새로고침을 대비한 sessionStorage 선작업 고민 중 ...

--- src/client/js/views/game/indianPocker/fns/gameState/statePlaying/playingEndData.js
+++ src/client/js/views/game/indianPocker/fns/gameState/statePlaying/playingEndData.js
@@ -14,42 +14,53 @@ import storageMethod from '@/client/js/module/storage/storageMethod';
 
 
 
-export default (_num) => {
+export default (_num, _clickBtn) => {
   return new Promise((resolve, reject) => {
     try {
-      // coins 관련 sessionStorage
-      const encryptKey1 = findCharCode([77, 76, 67, 88, 79, 87, 83, 90, 89, 86]); // extFirstBet
-      const encryptKey2 = findCharCode([72, 70, 85, 67, 83, 68, 89, 82, 77, 88]); // betUser
-      const encryptKey3 = findCharCode([83, 78, 84, 68, 66, 80, 71, 65, 67, 87]); // coinsEnemy
-      const encryptKey4 = findCharCode([67, 79, 66, 70, 75, 82, 74, 88, 69, 68]); // coinsEnemyBet
-      const encryptKey5 = findCharCode([80, 73, 68, 65, 90, 69, 88, 86, 82, 67]); // coinsEnemyExtBet
+      // 공통 sessionStorage
+      const encryptKey1 = findCharCode([77, 73, 75, 86, 85, 68, 75, 76, 87, 79, 68]); // gameState
 
-      // card 관련 sessionStorage
-      const encryptKey6 = findCharCode([65, 72, 66, 75, 85, 69, 87, 79, 88, 86]); // betResulting
-      const encryptKey7 = findCharCode([79, 76, 88, 84, 75, 65, 77, 73, 72, 86]); // drewFlipCardMode
-      const encryptKey8 = findCharCode([82, 67, 70, 69, 68, 86, 88, 74, 83, 78]); // drewReady
-      const encryptKey9 = findCharCode([81, 69, 71, 84, 85, 90, 82, 67, 77, 89]); // dropState
-      const encryptKey10 = findCharCode([90, 77, 71, 84, 65, 68, 87, 81, 70, 82]); // drewCardReady
-      const encryptKey11 = findCharCode([77, 87, 85, 88, 83, 80, 79, 90, 65, 66]); // playCardNum
-      const encryptKey12 = findCharCode([67, 71, 79, 68, 76, 73, 84, 74, 80, 77]); // drewState
-      const encryptKey13 = findCharCode([72, 70, 85, 67, 83, 68, 89, 82, 77, 88]); // betUser
+      // coins 관련 sessionStorage
+      const encryptKey2 = findCharCode([77, 76, 67, 88, 79, 87, 83, 90, 89, 86]); // extFirstBet
+      const encryptKey3 = findCharCode([72, 70, 85, 67, 83, 68, 89, 82, 77, 88]); // betUser
+      const encryptKey4 = findCharCode([83, 78, 84, 68, 66, 80, 71, 65, 67, 87]); // coinsEnemy
+      const encryptKey5 = findCharCode([67, 79, 66, 70, 75, 82, 74, 88, 69, 68]); // coinsEnemyBet
+      const encryptKey6 = findCharCode([80, 73, 68, 65, 90, 69, 88, 86, 82, 67]); // coinsEnemyExtBet
+
+      // card 관련 sessionStorage - coins 관련 sessionStorage 모두 사용됨
+      const encryptKey7 = findCharCode([65, 72, 66, 75, 85, 69, 87, 79, 88, 86]); // betResulting
+      const encryptKey8 = findCharCode([79, 76, 88, 84, 75, 65, 77, 73, 72, 86]); // drewFlipCardMode
+      const encryptKey9 = findCharCode([82, 67, 70, 69, 68, 86, 88, 74, 83, 78]); // drewReady
+      const encryptKey10 = findCharCode([81, 69, 71, 84, 85, 90, 82, 67, 77, 89]); // dropState
+      const encryptKey11 = findCharCode([90, 77, 71, 84, 65, 68, 87, 81, 70, 82]); // drewCardReady
+      const encryptKey12 = findCharCode([77, 87, 85, 88, 83, 80, 79, 90, 65, 66]); // playCardNum
+      const encryptKey13 = findCharCode([67, 71, 79, 68, 76, 73, 84, 74, 80, 77]); // drewState
       const encryptVal14 = findCharCode([90, 89, 80, 70, 68, 84, 65, 77, 74, 78]); // betUserFirst
       const encryptVal15 = findCharCode([83, 78, 86, 79, 68, 73, 71, 87, 82, 85]); // roundEnd
-      const encryptVal16 = findCharCode([77, 76, 67, 88, 79, 87, 83, 90, 89, 86]); // extFirstBet
-      const encryptKey17 = findCharCode([81, 67, 69, 68, 71, 77, 83, 90, 65, 74]); // coinsPlayer
-      const encryptKey18 = findCharCode([88, 79, 86, 74, 72, 80, 71, 70, 69, 77]); // coinsPlayerBet
-      const encryptKey19 = findCharCode([70, 90, 79, 67, 88, 77, 69, 82, 84, 81]); // coinsPlayerExtBet
-      const encryptKey20 = findCharCode([83, 78, 84, 68, 66, 80, 71, 65, 67, 87]); // coinsEnemy
-      const encryptKey21 = findCharCode([67, 79, 66, 70, 75, 82, 74, 88, 69, 68]); // coinsEnemyBet
-      const encryptKey22 = findCharCode([80, 73, 68, 65, 90, 69, 88, 86, 82, 67]); // coinsEnemyExtBet
-      const encryptKey23 = findCharCode([68, 85, 72, 73, 84, 65, 90, 70, 89, 88]); // betCoin
-      const encryptKey24 = findCharCode([68, 69, 75, 72, 67, 86, 90, 80, 65, 79]); // betCoinPos
-      const encryptKey25 = findCharCode([81, 69, 77, 72, 75, 67, 73, 87, 79, 74]); // basicBettingState
-      const encryptKey26 = findCharCode([70, 77, 80, 88, 87, 86, 83, 89, 75, 65]); // betState
-      const encryptVal27 = findCharCode([70, 84, 75, 87, 74, 67, 73, 77, 80, 65]); // basicBetting
+      const encryptKey16 = findCharCode([81, 67, 69, 68, 71, 77, 83, 90, 65, 74]); // coinsPlayer
+      const encryptKey17 = findCharCode([88, 79, 86, 74, 72, 80, 71, 70, 69, 77]); // coinsPlayerBet
+      const encryptKey18 = findCharCode([70, 90, 79, 67, 88, 77, 69, 82, 84, 81]); // coinsPlayerExtBet
+      const encryptKey19 = findCharCode([68, 85, 72, 73, 84, 65, 90, 70, 89, 88]); // betCoin
+      const encryptKey20 = findCharCode([68, 69, 75, 72, 67, 86, 90, 80, 65, 79]); // betCoinPos
+      const encryptKey20_1 = findCharCode([66, 85, 87, 74, 79, 90, 86, 83, 72, 88]); // betCoinPos : host
+      const encryptKey20_2 = findCharCode([85, 75, 72, 69, 71, 66, 74, 81, 87, 84]); // betCoinPos : translateX
+      const encryptKey20_3 = findCharCode([80, 67, 90, 85, 82, 71, 70, 66, 84, 74]); // betCoinPos : translateY
+      const encryptKey21 = findCharCode([81, 69, 77, 72, 75, 67, 73, 87, 79, 74]); // basicBettingState
+      const encryptKey22 = findCharCode([70, 77, 80, 88, 87, 86, 83, 89, 75, 65]); // betState
+      const encryptVal23 = findCharCode([70, 84, 75, 87, 74, 67, 73, 77, 80, 65]); // basicBetting
 
+      if (_clickBtn === 'call' || _clickBtn === 'fold') {
+        // 상대 카드번호 저장
+        storageMethod('s', 'SET_ITEM', encryptKey12, _num); // playCardNum
+      };
 
-      resolve();
+      const D = Object.create(null);
+
+      if (_clickBtn === 'call') {
+        //
+      };
+
+      resolve(data);
     } catch (error) {
       reject(error);
     }
