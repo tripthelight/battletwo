7b1a02f153547b71bf01a327eb8333ef6329bc01
tripthelight <bye_ok@naver.com>
2026-03-11 11:41:34 +0900

    [260311] - state gameOver work

--- src/client/js/views/game/blackAndWhite1/fns/gameState/statePlaying/showBattleResult.js
+++ src/client/js/views/game/blackAndWhite1/fns/gameState/statePlaying/showBattleResult.js
@@ -1,4 +1,6 @@
 import { timeInterval_1000 } from "@/client/js/functions/variable";
+import { dec } from '@/client/js/module/crypts/obf8lower';
+import returnResult from '@/client/js/views/game/blackAndWhite1/fns/common/returnResult';
 import scoreAssignment from "@/client/js/views/game/blackAndWhite1/fns/gameState/statePlaying/scoreAssignment";
 import countRound from "@/client/js/views/game/blackAndWhite1/fns/gameState/statePlaying/countRound";
 import resetCard from "@/client/js/views/game/blackAndWhite1/fns/gameState/statePlaying/resetCard";
@@ -10,7 +12,7 @@ import setGameOrderRound from "@/client/js/views/game/blackAndWhite1/fns/gameSta
 export default (res) => {
   const elem = document.createElement("div");
   elem.classList.add("show-battle-result");
-  elem.classList.add(res);
+  elem.classList.add(returnResult(dec(res)));
   const CONTAINER_EL = document.getElementById("container");
   if (CONTAINER_EL) {
     CONTAINER_EL.appendChild(elem);
